8e13502e055308525e695656a4676b9fed3db212
Erik Deijl <erikdeijl@Eriks-MacBook-Pro.local>
2016-01-22 12:10:04 +0100

    fix the loop

--- src/Reactive/Banana/SDL2.hs
+++ src/Reactive/Banana/SDL2.hs
@@ -17,33 +17,31 @@ import qualified SDL.Raw                    as SDLR
 getSDLEventSource :: IO SDLEventSource
 getSDLEventSource = SDLEventSource <$> newAddHandler <*> newAddHandler
 
+-- | one step in the main event loop, returning False when it needs to stop
+
 mainSDLPump :: SDLEventSource -> IO Bool
 mainSDLPump es= do
   let esdl = getSDLEvent es
       etick = getTickEvent es
   tick <- SDL.ticks
   me <- collectEvents
+
   case me of
     Nothing -> return False
     Just e -> do
-      fire esdl e
+      mapM (fire esdl) e
       fire etick tick
       return True
-  --case me of
 
-
-    --Nothing -> return False
-    --e -> do
-      --fire esdl e
-      --fire etick tick
-      --return True
-
-collectEvents :: IO (Maybe SDL.EventPayload)
+-- | collect SDL events
+-- return Nothing on quit, otherwise the last event
+collectEvents :: IO (Maybe [SDL.EventPayload])
 collectEvents = do
-  e <- SDL.pollEvent
-  case fmap eventPayload e of
-    Just SDL.QuitEvent -> return Nothing
-    Just ed -> return (Just ed)
+      e <- SDL.pollEvent
+      case fmap eventPayload e of
+        Just SDL.QuitEvent -> return Nothing
+        Nothing -> return (Just [])
+        Just ev -> liftM (liftM (ev:)) collectEvents
 
 runSDLPump :: SDLEventSource -> IO ()
 runSDLPump es = whileM (mainSDLPump es)
